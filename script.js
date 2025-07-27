fbd2b1c08fa1cdf03df193912fc2e0846b8ccd3f
Adam <adaaaaamalaoui@gmail.com>
2025-07-27 04:30:19 +0100

    added the burger thingy for mobile

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleTheme() {
   document.documentElement.classList.toggle('dark-theme');
   document.documentElement.classList.toggle('light-theme');
 }
+
+function toggleMenu() {
+  const nav = document.getElementById('mobileNav');
+  nav.classList.toggle('show');
+}
